152e110f745ea9c1c2abe35bb55a96a4c42d3986
Michele Pratusevich <michele.pratusevich@gmail.com>
2018-10-09 13:24:33 -0400

    Build on TX2

--- nv_jtx1_build_and_install_vgl_and_vnc.sh
+++ nv_jtx1_build_and_install_vgl_and_vnc.sh
@@ -22,68 +22,72 @@ sudo apt-get install libssl-dev
 
 # Build and install libjpeg-turbo
 git clone https://github.com/libjpeg-turbo/libjpeg-turbo.git
-mkdir libjpeg-turbo-build
 cd libjpeg-turbo
+git checkout 1.5.1
+mkdir build
 autoreconf -fiv
-cd ../libjpeg-turbo-build
-sh ../libjpeg-turbo/configure
-make
+cd build
+sh ../configure
+make -j4
 # Change "DEBARCH=aarch64" to "DEBARCH=arm64"
 sed -i 's/aarch64/arm64/g' pkgscripts/makedpkg.tmpl
 make deb
-# sudo dpkg -i libjpeg-turbo_1.5.2_arm64.deb
-cd ../
+sudo dpkg -i libjpeg-turbo_1.5.1_arm64.deb
+cd ../../
 
 # VIRTUALGL
 
 # Preventing link error from "libGL.so", check this:
 # https://devtalk.nvidia.com/default/topic/946136/jetson-tx1/building-an-opengl-application/
-cd /usr/lib/aarch64-linux-gnu
-sudo rm libGL.so
-sudo ln -s /usr/lib/aarch64-linux-gnu/tegra/libGL.so libGL.so
+# confirm that /usr/lib/aarch64-linux-gnu/libGL.so links to /usr/lib/aarch64-linux-gnu/tegra/libGL.so
+# cd /usr/lib/aarch64-linux-gnu
+# sudo rm libGL.so
+# sudo ln -s /usr/lib/aarch64-linux-gnu/tegra/libGL.so libGL.so
 
 # Build and install VirtualGL
 cd $currentDir
 git clone https://github.com/VirtualGL/virtualgl.git
-mkdir virtualgl-build
-cd virtualgl-build
-cmake -G "Unix Makefiles" -DTJPEG_LIBRARY="-L/opt/libjpeg-turbo/lib64/ -lturbojpeg" ../virtualgl
-make
+cd virtualgl
+git checkout 2.5.x
+mkdir build
+cd build
+cmake -G "Unix Makefiles" -DTJPEG_LIBRARY="-L/opt/libjpeg-turbo/lib64/ -lturbojpeg" ..
+make -j4
 # Change "DEBARCH=aarch64" to "DEBARCH=arm64"
 sed -i 's/aarch64/arm64/g' pkgscripts/makedpkg
 # Change "Architecture: aarch64" to "Architecture: arm64"
 sed -i 's/aarch64/arm64/g' pkgscripts/deb-control
 make deb
-# sudo dpkg -i sudo dpkg -i virtualgl_2.5.2_arm64.deb
-cd ..
+sudo dpkg -i virtualgl_2.5.3_arm64.deb
+cd ../../
 
 # TURBOVNC
 
 # Build and install TurboVNC
 git clone https://github.com/TurboVNC/turbovnc.git
-
-mkdir turbovnc-build
-cd turbovnc-build
-cmake -G "Unix Makefiles" -DTVNC_BUILDJAVA=0 -DTJPEG_LIBRARY="-L/opt/libjpeg-turbo/lib64/ -lturbojpeg" ../turbovnc
+cd turbovnc
+mkdir build
+cd build
+cmake -G "Unix Makefiles" -DTVNC_BUILDJAVA=0 -DTJPEG_LIBRARY="-L/opt/libjpeg-turbo/lib64/ -lturbojpeg" ..
 
 # Prevent error like #error "GLYPHPADBYTES must be 4",
 # edit ../turbovnc/unix/Xvnc/programs/Xserver/include/servermd.h
 # and prepend before "#ifdef __avr32__"
-servermd="$currentDir/turbovnc/unix/Xvnc/programs/Xserver/include/servermd.h"
-line="#ifdef __avr32__"
-defs="#ifdef __aarch64__\n\
+# servermd="$currentDir/turbovnc/unix/Xvnc/programs/Xserver/include/servermd.h"
+# line="#ifdef __avr32__"
+# defs="#ifdef __aarch64__\n\
 # define IMAGE_BYTE_ORDER       LSBFirst\n\
 # define BITMAP_BIT_ORDER       LSBFirst\n\
 # define GLYPHPADBYTES          4\n\
 #endif\n"
-sed -i "/$line/i $defs" "$servermd"
-make
+# sed -i "/$line/i $defs" "$servermd"
+make -j4
 # Change "DEBARCH=aarch64" to "DEBARCH=arm64"
 sed -i 's/aarch64/arm64/g' pkgscripts/makedpkg
 # Change "Architecture: aarch64" to "Architecture: arm64"
 sed -i 's/aarch64/arm64/g' pkgscripts/deb-control
 make deb
-# sudo dpkg -i turbovnc_2.1.1_arm64.deb
+sudo dpkg -i turbovnc_2.2_arm64.deb
 
 # SYSTEM
 
@@ -102,29 +106,4 @@ fi
 # Add VirtualGL to path
 if ! grep -Fq "/opt/VirtualGL/bin" "$HOME/.bashrc"; then
     echo 'export PATH=$PATH:/opt/VirtualGL/bin' >> ~/.bashrc
-fi
-
-# copied from https://github.com/nicksp/dotfiles/blob/master/setup.sh
-answer_is_yes() {
-  [[ "$REPLY" =~ ^[Yy]$ ]] \
-    && return 0 \
-    || return 1
-}
-
-print_question() {
-  # Print output in yellow
-  printf "\e[0;33m  [?] $1\e[0m"
-}
-
-# copied from https://github.com/nicksp/dotfiles/blob/master/setup.sh
-ask_for_confirmation() {
-  print_question "$1 (y/n) "
-  read -n 1
-  printf "\n"
-}
-
-cd ..
-ask_for_confirmation "Do you want to remove leftover files?"
-if answer_is_yes; then
-    rm -drf tmp
 fi
